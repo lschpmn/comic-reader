d69c7119f19771d662982f35fdae3f68794a2453
lschpmn <luis@lmc-productions.com>
2021-04-17 13:47:09 -0500

    Adding some basic styling

--- client/App.tsx
+++ client/App.tsx
@@ -1,17 +1,22 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 const App = () => {
-  return <div style={styles.container}>
+  const styles = useStyles();
+
+  return <div className={styles.container}>
     <h1>Hello World</h1>
-  </div>
+  </div>;
 };
 
-const styles = {
+const useStyles = makeStyles(({ palette }) => ({
   container: {
+    backgroundColor: palette.background.default,
+    color: palette.text.primary,
     height: '100%',
     overflow: 'auto',
   },
-};
+}));
 
 export default hot(App);
