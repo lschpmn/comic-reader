138ecdf892f8751642c97e4b3738601af5b4a809
lschpmn <luis@lmc-productions.com>
2021-04-19 20:54:22 -0500

    Added file tree side panel

---
+++ client/components/FileTreeSidePanel.tsx
@@ -0,0 +1,8 @@
+import Paper from '@material-ui/core/Paper';
+import React from 'react';
+
+export default () => {
+  return <Paper style={{ width: '15rem' }}>
+    <h3 style={{ margin: 0 }}>Files!</h3>
+  </Paper>
+};
--- client/components/SideMenu.tsx
+++ client/components/SideMenu.tsx
@@ -3,21 +3,28 @@ import Paper from '@material-ui/core/Paper';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import AccountTreeIcon from '@material-ui/icons/AccountTree';
 import SettingsIcon from '@material-ui/icons/Settings';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import FileTreeSidePanel from './FileTreeSidePanel';
 
 export default () => {
+  const [openTree, setOpenTree] = useState(false);
   const styles = useStyles();
-  console.log(styles);
 
-  return <Paper className={styles.container}>
-    <IconButton className={styles.icons} size="small">
-      <AccountTreeIcon fontSize="small"/>
-    </IconButton>
-    <span style={{ flex: 1 }}/>
-    <IconButton className={styles.icons} size="small">
-      <SettingsIcon fontSize="small"/>
-    </IconButton>
-  </Paper>;
+  const onTreeClick = useCallback(() => setOpenTree(!openTree), [openTree]);
+
+  return <div style={{ display: 'flex' }}>
+    {openTree && <FileTreeSidePanel />}
+
+    <Paper className={styles.container}>
+      <IconButton className={styles.icons} size="small" onClick={onTreeClick}>
+        <AccountTreeIcon fontSize="small"/>
+      </IconButton>
+      <span style={{ flex: 1 }}/>
+      <IconButton className={styles.icons} size="small">
+        <SettingsIcon fontSize="small"/>
+      </IconButton>
+    </Paper>
+  </div>;
 };
 
 const useStyles = makeStyles(({ palette }) => ({
