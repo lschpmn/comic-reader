f2ea05ac63461676db2516ab9ae9439f3f43fc62
lschpmn <luis@lmc-productions.com>
2021-04-19 22:48:59 -0500

    WIP Adding File Tree

--- client/components/FileTreeSidePanel.tsx
+++ client/components/FileTreeSidePanel.tsx
@@ -1,8 +1,22 @@
 import Paper from '@material-ui/core/Paper';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import TreeItem from '@material-ui/lab/TreeItem';
+import TreeView from '@material-ui/lab/TreeView';
 import React from 'react';
 
 export default () => {
   return <Paper style={{ width: '15rem' }}>
     <h3 style={{ margin: 0 }}>Files!</h3>
-  </Paper>
+    <TreeView
+      disableSelection
+      defaultCollapseIcon={<ExpandMoreIcon/>}
+      defaultExpandIcon={<ChevronRightIcon/>}
+      defaultEndIcon={<ChevronRightIcon/>}
+    >
+      <TreeItem nodeId="1" label="test">
+        <TreeItem nodeId="2" label="inside" onClick={() => console.log('clicked')}/>
+      </TreeItem>
+    </TreeView>
+  </Paper>;
 };
